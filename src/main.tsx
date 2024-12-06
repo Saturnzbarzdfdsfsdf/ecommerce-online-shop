6e9c1cce066c68566ad64e3f24cdd8f630643108
Timur <saturnianin777@gmail.com>
2024-12-06 19:10:09 +0300

    browser -> HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,22 @@
-import { createRoot } from 'react-dom/client'
+import { createRoot } from 'react-dom/client';
 
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom';
 
-import { Provider } from 'react-redux'
+import { Provider } from 'react-redux';
 
-import { store } from './redux/store.ts'
+import { store } from './redux/store.ts';
 
-import App from './app/App.tsx'
+import App from './app/App.tsx';
 
-import './shared/styles/index.css'
-import { StrictMode } from 'react'
+import './shared/styles/index.css';
+import { StrictMode } from 'react';
 
 createRoot(document.getElementById('root')!).render(
-	<BrowserRouter
-		future={{
-			v7_relativeSplatPath: true,
-		}}
-	>
+	<HashRouter>
 		<Provider store={store}>
 			<StrictMode>
 				<App />
 			</StrictMode>
 		</Provider>
-	</BrowserRouter>
-)
+	</HashRouter>
+);
