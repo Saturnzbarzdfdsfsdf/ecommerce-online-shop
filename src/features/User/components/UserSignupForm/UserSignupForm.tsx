97434c4fabc04eb45ec518ba07d6638e64f99fa8
Timur <saturnianin777@gmail.com>
2024-12-07 12:13:04 +0300

    after refactor user

--- src/features/User/components/UserSignupForm/UserSignupForm.tsx
+++ src/features/User/components/UserSignupForm/UserSignupForm.tsx
@@ -25,69 +25,65 @@ const UserSignupForm: React.FC<IUserFormProps> = ({
 	};
 
 	return (
-			<form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
-				<div className={styles.group}>
+		<form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+			<div className={styles.group}>
+				<input
+					type='email'
+					placeholder='Your email'
+					autoComplete='off'
+					{...register('email', {
+						required: 'Email is required',
+						pattern: {
+							value: /\S+@\S+\.\S+/,
+							message: 'Invalid email address',
+						},
+					})}
+				/>
+				{errors.email && <div>{errors.email.message}</div>}
+			</div>
 
-					<input
-						type='text'
-						placeholder='Your name'
-						autoComplete='off'
-						{...register('name', { required: 'Name is required' })}
-					/>
-					{errors.name && <div>{errors.name.message}</div>}
-				</div>
+			<div className={styles.group}>
+				<input
+					type='text'
+					placeholder='Your name'
+					autoComplete='off'
+					{...register('name', { required: 'Name is required' })}
+				/>
+				{errors.name && <div>{errors.name.message}</div>}
+			</div>
 
-				<div className={styles.group}>
+			<div className={styles.group}>
+				<input
+					type='password'
+					placeholder='Your password'
+					autoComplete='off'
+					{...register('password', { required: 'Password is required' })}
+				/>
+				{errors.password && <div>{errors.password.message}</div>}
+			</div>
 
-					<input
-						type='email'
-						placeholder='Your email'
-						autoComplete='off'
-						{...register('email', {
-							required: 'Email is required',
-							pattern: {
-								value: /\S+@\S+\.\S+/,
-								message: 'Invalid email address',
-							},
-						})}
-					/>
-					{errors.email && <div>{errors.email.message}</div>}
-				</div>
+			<div className={styles.group}>
+				<input
+					type='text'
+					// name='avatar'
+					placeholder='Your avatar URL'
+					autoComplete='off'
+					defaultValue='https://images.panda.org/assets/images/pages/welcome/orangutan_1600x1000_279157.jpg'
+					{...register('avatar', { required: 'Avatar URL is required' })}
+				/>
+				{errors.avatar && <div>{errors.avatar.message}</div>}
+			</div>
 
-				<div className={styles.group}>
-
-					<input
-						type='password'
-						placeholder='Your password'
-						autoComplete='off'
-						{...register('password', { required: 'Password is required' })}
-					/>
-					{errors.password && <div>{errors.password.message}</div>}
-				</div>
-
-				<div className={styles.group}>
-
-					<input
-						type='text'
-						// name='avatar'
-						placeholder='Your avatar URL'
-						autoComplete='off'
-						defaultValue='https://images.panda.org/assets/images/pages/welcome/orangutan_1600x1000_279157.jpg'
-						{...register('avatar', { required: 'Avatar URL is required' })}
-					/>
-					{errors.avatar && <div>{errors.avatar.message}</div>}
-				</div>
-
-				<div
-					className={styles.link}
-					onClick={() => toggleCurrentFormType('login')}
-				>
-					I already have an account
-				</div>
-				<button type='submit' className={styles.submit}>
-					Create an account
-				</button>
-			</form>
+			<div
+				className={styles.link}
+				onClick={() => toggleCurrentFormType('login')}
+			>
+				I already have an account
+			</div>
+			<button type='submit' className={styles.submit}>
+				Create an account
+			</button>
+		</form>
 	);
 };
 
--- src/widget/User/ui/UserSignup.tsx
+++ src/widget/User/ui/UserSignup.tsx
@@ -1,46 +1,14 @@
-import React from 'react';
+import { FC } from 'react';
 
-import { createUser } from '../../../redux/user/userSlice';
-
-import { useAppDispatch } from '../../../redux/store';
-
-import { IUserFormProps, IFormUserValues } from '../../../shared/types';
+import { IUserFormProps } from '../../../shared/types';
+import { UserSignupForm } from '../../../features/User/index';
 
 import styles from './User.module.css';
 
-const UserSignupForm: React.FC<IUserFormProps> = ({
+const UserSignup: FC<IUserFormProps> = ({
 	toggleCurrentFormType,
 	closeForm,
 }) => {
-	const dispatch = useAppDispatch();
-
-	const [values, setValue] = React.useState<IFormUserValues>({
-		name: '',
-		email: '',
-		password: '',
-		avatar:
-			'https://images.panda.org/assets/images/pages/welcome/orangutan_1600x1000_279157.jpg',
-	});
-
-	// Обработка изменений полей ввода
-	const handleChange = ({
-		target: { value, name },
-	}: React.ChangeEvent<HTMLInputElement>) => {
-		setValue({ ...values, [name]: value });
-	};
-
-	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-		e.preventDefault();
-		// Проверка на заполненность полей
-		const isNotEmpty = Object.values(values).every(val => val);
-		// Если поля не заполнены, то выйти из кода
-		if (!isNotEmpty) return;
-
-		// Если заполнены, то отправляю в redux
-		dispatch(createUser(values));
-		closeForm();
-	};
-
 	return (
 		<div className={styles.wrapper}>
 			<div onClick={closeForm} className={styles.close}>
@@ -51,67 +19,12 @@ const UserSignupForm: React.FC<IUserFormProps> = ({
 
 			<div className={styles.title}>Sign UP</div>
 
-			<form className={styles.form} onSubmit={handleSubmit}>
-				<div className={styles.group}>
-					<input
-						onChange={handleChange}
-						value={values.email}
-						type='email'
-						name='email'
-						placeholder='Your email'
-						autoComplete='off'
-						required
-					/>
-				</div>
-
-				<div className={styles.group}>
-					<input
-						onChange={handleChange}
-						value={values.name}
-						type='text'
-						name='name'
-						placeholder='Your name'
-						autoComplete='off'
-						required
-					/>
-				</div>
-
-				<div className={styles.group}>
-					<input
-						onChange={handleChange}
-						value={values.password}
-						name='password'
-						type='password'
-						placeholder='Your password'
-						autoComplete='off'
-						required
-					/>
-				</div>
-
-				<div className={styles.group}>
-					<input
-						onChange={handleChange}
-						value={values.avatar}
-						type='text'
-						name='avatar'
-						placeholder='Your avatar'
-						autoComplete='off'
-						required
-					/>
-				</div>
-
-				<div
-					className={styles.link}
-					onClick={() => toggleCurrentFormType('login')}
-				>
-					I already have an account
-				</div>
-				<button type='submit' className={styles.submit}>
-					Create an account
-				</button>
-			</form>
+			<UserSignupForm
+				toggleCurrentFormType={toggleCurrentFormType}
+				closeForm={closeForm}
+			/>
 		</div>
 	);
 };
 
-export default UserSignupForm;
+export default UserSignup;
