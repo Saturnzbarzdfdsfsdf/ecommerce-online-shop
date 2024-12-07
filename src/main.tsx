a7c897cd4c8a484520487abe127bf85025279110
Timur <saturnianin777@gmail.com>
2024-12-07 08:19:29 +0300

    befote gh-pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import { Provider } from 'react-redux';
 
@@ -9,14 +10,13 @@ import { store } from './redux/store.ts';
 import App from './app/App.tsx';
 
 import './shared/styles/index.css';
-import { StrictMode } from 'react';
 
 createRoot(document.getElementById('root')!).render(
-	<HashRouter>
+	<BrowserRouter>
 		<Provider store={store}>
 			<StrictMode>
 				<App />
 			</StrictMode>
 		</Provider>
-	</HashRouter>
+	</BrowserRouter>
 );
